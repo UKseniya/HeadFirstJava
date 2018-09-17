84763cf99983a945eca1295264dedcb53421a0e9
Kseniya <kseniya.utarbekova@gmail.com>
2018-09-17 18:04:43 +0500

    added Exercise 1

---
+++ Chapter1/src/main/java/kz/kstu/hf/chapter1/Main.java
@@ -0,0 +1,11 @@
+package kz.kstu.hf.chapter1;
+
+
+public class Main {
+    private static final int MEASURE_COUNT = 1;
+
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+
+}
