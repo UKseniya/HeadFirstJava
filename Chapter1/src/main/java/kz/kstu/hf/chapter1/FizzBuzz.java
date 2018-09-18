48642581905a89b8e8c2c3ed8e34cf1ec96d1b47
Kseniya <kseniya.utarbekova@gmail.com>
2018-09-18 09:16:32 +0500

    added FizzBuzz Test

---
+++ Chapter1/src/main/java/kz/kstu/hf/chapter1/FizzBuzz.java
@@ -0,0 +1,28 @@
+package kz.kstu.hf.chapter1;
+
+public class FizzBuzz {
+    public static void main (String [] args){
+
+        int x = 1;
+
+        while (x < 100) {
+
+            if (x%15 == 0) {
+                System.out.println("FizzBuzz");
+            }
+
+            else if (x%5 == 0) {
+                System.out.println("Buzz");
+            }
+
+            else if (x%3 == 0){
+                System.out.println("Fizz");
+            }
+            else {
+                System.out.println(x);
+            }
+            x = x+1;
+
+        }
+    }
+}
